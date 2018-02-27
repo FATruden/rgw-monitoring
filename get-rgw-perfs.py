8947d404aff83ea4b4dd11ef69d46cb0a70c49ac
Aleksandr Rudenko <a.rudikk@gmail.com>
2018-02-27 20:41:56 +0300

    tmp

--- get-rgw-perfs.py
+++ get-rgw-perfs.py
@@ -1,19 +1,17 @@
 #!/bin/env python
 
 import json
+import glob
 import os
-import platform
 import subprocess
 import sys
 
 
-hostname = platform.node().split(".")[0]
-rgw_name = "client.radosgw.{0}".format(hostname)
-rgw_sock = "/var/run/ceph/ceph-{0}.asok".format(rgw_name)
-save_path = "/tmp"
-file_prefix = "rgw"
+SOCK_DIR = "/var/run/ceph"
+RGW_SOCK_PREF = "ceph-client.*"
+SAVE_PATH = "/tmp"
 
-keys = [
+MONITORING_KEYS = [
     "req",
     "failed_req",
     "get",
@@ -27,14 +25,28 @@ keys = [
 ]
 
 
-def get_metrics():
-    """Get all metrics from RGW and return their as JSON"""
+def get_rgw_instances():
+    """
+    Reterns list of rgw/rwb for monitoring like:
+    [{'asock': '/var/run/ceph/ceph-client.rgw.a.asok', 'type': 'rgw'}]
+    """
+
+    active_sockets = glob.glob(os.path.join(SOCK_DIR, RGW_SOCK_PREF))
+
+    instances = []
+    for sock in active_sockets:
+        instances.append(
+            {"type": sock.split(".")[1], "id": sock.split(".")[2], "asock": sock})
+
+    return instances
+
+
+def get_metrics(admin_sock):
+    """Returns JSON of all RGW/RWB metrics."""
 
     try:
-        raw_metrics = subprocess.Popen(["ceph",
-            "--format", "json",
-            "--admin-daemon", rgw_sock,
-            "perf", "dump"], stdout=subprocess.PIPE)
+        raw_metrics = subprocess.Popen(["ceph", "--format", "json",
+            "--admin-daemon", admin_sock, "perf", "dump"], stdout=subprocess.PIPE)
     except subprocess.CalledProcessError as e:
         sys.exit("Error: {0}".format(e))
 
@@ -44,7 +56,7 @@ def get_metrics():
         sys.exit("Error: {0}".format(e))
 
 
-def write_res(filename, result):
+def write_result(filename, result):
     """Write metric to file."""
 
     f = open(filename, 'w')
@@ -52,12 +64,12 @@ def write_res(filename, result):
     f.close()
 
 
-def get_latency(metrics, latency_type):
-    """Calculate latency betwen checks"""
+def get_latency(metrics, rgw_type, rgw_name, latency_type):
+    """Calculate latency betwen checks."""
 
-    metric_file = "{0}/{1}.{2}".format(save_path, file_prefix, latency_type)
-    last_metrics_file = "{0}/last_{1}.{2}".format(save_path,
-        file_prefix, latency_type)
+    metric_file = "{0}/{1}.{2}".format(SAVE_PATH, rgw_type, latency_type)
+    last_metrics_file = "{0}/last_{1}.{2}".format(
+        SAVE_PATH, rgw_type, latency_type)
 
     current_obj_count = float(metrics[rgw_name][latency_type]["avgcount"])
     current_lat_sum = float(metrics[rgw_name][latency_type]["sum"])
@@ -71,28 +83,34 @@ def get_latency(metrics, latency_type):
         lat_sum = current_lat_sum - float(last_lat_sum)
 
         if obj_count > 0.0:
-            latency = lat_sum / obj_count
-            write_res(metric_file, latency)
-            write_res(last_metrics_file, "{0} {1}".format(
-                current_obj_count, current_lat_sum))
-
+            latency = round(lat_sum / obj_count, 3)
+            write_result(metric_file, latency)
+        else:
+            write_result(metric_file, 0)
     else:
-        write_res(last_metrics_file, "{0} {1}".format(
-            current_obj_count, current_lat_sum))
-        write_res(metric_file, 0)
+        latency = round(current_lat_sum / current_obj_count, 3)
+        write_result(metric_file, latency)
+
+    write_result(last_metrics_file, "{0} {1}".format(
+        current_obj_count, current_lat_sum))
 
 
 def main():
 
-    metrics = get_metrics()
+    for rgw in get_rgw_instances():
+        rgw_name = "client.{0}.{1}".format(rgw["type"], rgw["id"])
+        metrics = get_metrics(rgw["asock"])
 
-    for key in keys:
-        metric = metrics[rgw_name][key]
-        filename = "{0}/{1}.{2}".format(save_path, file_prefix, key)
-        write_res(filename, metric)
+        for key in MONITORING_KEYS:
+            metric = metrics[rgw_name][key]
+            filename = "{0}/{1}.{2}".format(SAVE_PATH, rgw["type"], key)
+            write_result(filename, metric)
 
-    get_latency(metrics, "get_initial_lat")
-    get_latency(metrics, "put_initial_lat")
+        get_latency(metrics, rgw["type"], rgw_name, "get_initial_lat")
+
+        # RWB doesn't have put metrics
+        if rgw["type"] == "rgw":
+            get_latency(metrics, rgw["type"], rgw_name, "put_initial_lat")
 
 
 if __name__ == "__main__":
